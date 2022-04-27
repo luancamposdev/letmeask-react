c7590f2623f6286b6ff16b67a7df0ce46471bcd5
Luan Campos <luancamposproducoes@gmail.com>
2022-04-27 11:06:12 -0300

    create screen Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,8 @@ import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
 import googleIconImg from "../assets/images/google-icon.svg";
 
+import Button from "../components/Button";
+
 import "../styles/auth.scss";
 
 export function Home() {
@@ -16,18 +18,18 @@ export function Home() {
             <main>
                 <div className="main-content">
                     <img src={logoImg} alt="Let-me AsK" />
-                    <button>
-                        <img src={googleIconImg} alt="Log0o do Google" />
+                    <button className="create-room">
+                        <img src={googleIconImg} alt="Logo do Google" />
                         Crie sua sala com o Google
                     </button>
-                    <div>Ou entre em uma sala</div>
+                    <div className="separator">Ou entre em uma sala</div>
 
                     <form>
                         <input
                             type="text"
                             placeholder="Digiete o código da sala"
                         />
-                        <button type="submit">Entrar na Sala</button>
+                        <Button type="submit">Entrar na Sala</Button>
                     </form>
                 </div>
             </main>
