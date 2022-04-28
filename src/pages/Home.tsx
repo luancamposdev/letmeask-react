ab276e4855e3e331e1c351f0ed4c7c0f951160ac
Luan Campos <luancamposproducoes@gmail.com>
2022-04-27 23:05:31 -0300

    add permissions and auth finish

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,14 @@
+import { FormEvent, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import Button from "../components/Button";
 import { useAuth } from "../hooks/useAuth";
+import { database } from "../services/firebase";
 
 import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
 import googleIconImg from "../assets/images/google-icon.svg";
+import LoginIcon from "../assets/images/login.png";
 
 import "../styles/auth.scss";
 
@@ -13,12 +16,30 @@ export function Home() {
     const navigate = useNavigate();
     const { user, signInWithGoogle } = useAuth();
 
+    const [roomCode, setRoomCode] = useState("");
+
     async function handleCreateRoom() {
         if (!user) {
             await signInWithGoogle();
         }
         navigate("/rooms/new");
     }
+
+    async function handleJoinRoom(event: FormEvent) {
+        event.preventDefault();
+
+        if (roomCode.trim() === "") return;
+
+        const roomRef = await database.ref(`/rooms/${roomCode}`).get();
+
+        if (!roomRef.exists()) {
+            alert(`Sala não existe.`);
+            return;
+        }
+
+        navigate(`/rooms/${roomCode}`);
+    }
+
     return (
         <div id="page-auth">
             <aside>
@@ -36,12 +57,19 @@ export function Home() {
                     </button>
                     <div className="separator">Ou entre em uma sala</div>
 
-                    <form>
+                    <form onSubmit={handleJoinRoom}>
                         <input
                             type="text"
-                            placeholder="Digiete o código da sala"
+                            placeholder="Digite o código da sala"
+                            onChange={(event) =>
+                                setRoomCode(event.target.value)
+                            }
+                            value={roomCode}
                         />
-                        <Button type="submit">Entrar na Sala</Button>
+                        <Button type="submit">
+                            Entrar na Sala
+                            <img src={LoginIcon} alt="Entrar na Sala" />
+                        </Button>
                     </form>
                 </div>
             </main>
